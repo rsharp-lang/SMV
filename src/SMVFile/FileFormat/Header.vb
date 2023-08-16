65396d03268a220d09bc8bb05d1b828f10bf6576
guigang xie <xie.guigang@gcmodeller.org>
2023-08-16 10:36:28 +0800

    parse header file

--- src/SMVFile/FileFormat/Header.vb
+++ src/SMVFile/FileFormat/Header.vb
@@ -2,6 +2,11 @@
 
     Public Class Header
 
+        Sub New()
+        End Sub
 
+        Public Shared Function Load(metadata As Dictionary(Of String, String)) As Header
+
+        End Function
     End Class
 End Namespace
--- src/SMVFile/FileFormat/Reader.vb
+++ src/SMVFile/FileFormat/Reader.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.Text.Parser
 
 Namespace FileFormat
 
@@ -7,7 +10,8 @@ Namespace FileFormat
 
         ReadOnly buf As BinaryDataReader
         ReadOnly header_size As Integer
-        ReadOnly header As Header
+
+        Dim header As Header
 
         Sub New(file As Stream)
             buf = New BinaryDataReader(file)
@@ -23,7 +27,31 @@ Namespace FileFormat
         End Function
 
         Private Sub loadHeaderInternal()
+            Dim c As Char
+            Dim metadata As New Dictionary(Of String, String)
+            Dim str As New CharBuffer
+            Dim tuple As NamedValue(Of String)
 
+            Do While True
+                c = buf.ReadChar
+
+                If c <> ASCII.CR AndAlso c <> ASCII.LF Then
+                    str += c
+                Else
+                    If str = "{" OrElse str = "}" Then
+                        str *= 0
+                    Else
+                        tuple = str.ToString.GetTagValue("=", trim:=True)
+                        metadata.Add(tuple.Name, tuple.Value)
+                    End If
+                End If
+
+                If buf.Position = header_size AndAlso c = "}"c Then
+                    Exit Do
+                End If
+            Loop
+
+            header = Header.Load(metadata)
         End Sub
     End Class
 End Namespace
