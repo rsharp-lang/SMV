72c51bea4d14eeefa70c9e1627849a6783f25d5e
guigang xie <xie.guigang@gcmodeller.org>
2023-08-16 11:14:45 +0800

    run debug test of the image display

--- test/readfile.R
+++ test/readfile.R
@@ -1,4 +1,5 @@
 require(SMV);
+require(graphics2D);
 
 setwd(@dir);
 
@@ -6,3 +7,12 @@ let data = open.smv_file("../data/ALS831_lyso_Gd_001.img");
 
 str(smv_header(data));
 
+let raster = smv_raster(data);
+
+print(raster);
+
+bitmap(file = "./raster_image_heatmap.png", size = [raster]::dimensionSize);
+
+graphics2D::rasterHeatmap(raster, colorName = "viridis");
+
+dev.off();
