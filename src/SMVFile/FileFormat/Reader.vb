da5b34a93655db081133d81c4a92173004d0c901
guigang xie <xie.guigang@gcmodeller.org>
2023-08-16 10:24:50 +0800

    how to parse the smv file headers?

---
+++ src/SMVFile/FileFormat/Reader.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Data.IO
+
+Namespace FileFormat
+
+    Public Class Reader
+
+        ReadOnly buf As BinaryDataReader
+        ReadOnly header_size As Integer
+        ReadOnly header As Header
+
+        Sub New(file As Stream)
+            buf = New BinaryDataReader(file)
+        End Sub
+
+        Public Function ReadHeader() As Header
+            If header Is Nothing Then
+                Call buf.Seek(Scan0, SeekOrigin.Begin)
+                Call loadHeaderInternal()
+            End If
+
+            Return header
+        End Function
+
+        Private Sub loadHeaderInternal()
+
+        End Sub
+    End Class
+End Namespace
--- src/SMVFile/Rscript.vb
+++ src/SMVFile/Rscript.vb
@@ -1,14 +1,31 @@
 
+Imports System.IO
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Internal.Object.Converts
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports SMVFile.FileFormat
 
 <Package("Reader")>
 Public Module Rscript
 
     <ExportAPI("open.smv_file")>
-    Public Function openSMVFile(<RRawVectorArgument> file As Object, Optional env As Environment = Nothing)
+    <RApiReturn(GetType(Reader))>
+    Public Function openSMVFile(<RRawVectorArgument> file As Object, Optional env As Environment = Nothing) As Object
+        Dim buf = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Read, env)
 
+        If buf Like GetType(Message) Then
+            Return buf.TryCast(Of Message)
+        End If
+
+        Return New Reader(buf.TryCast(Of Stream))
+    End Function
+
+    <ExportAPI("smv_header")>
+    Public Function GetFileHeaders(file As Reader, Optional env As Environment = Nothing) As Object
+        Return RConversion.asList(file.ReadHeader, list.empty, env)
     End Function
 End Module
