bf529ab8520ffb6b16c8602273295327f5a825bd
guigang xie <xie.guigang@gcmodeller.org>
2023-08-16 10:51:37 +0800

    read smv header data

--- src/SMVFile/FileFormat/Header.vb
+++ src/SMVFile/FileFormat/Header.vb
@@ -1,12 +1,66 @@
-﻿Namespace FileFormat
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.DataFramework
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports any = Microsoft.VisualBasic.Scripting
+
+Namespace FileFormat
 
     Public Class Header
+
+        Public Property HEADER_BYTES As Integer
+        Public Property [DIM] As Integer
+        Public Property BYTE_ORDER As String
+        Public Property TYPE As String
+        Public Property SIZE1 As Integer
+        Public Property SIZE2 As Integer
+        Public Property PIXEL_SIZE As Double
+        Public Property BIN As String
+        Public Property BIN_TYPE As String
+        Public Property ADC As String
+        Public Property CREV As Double
+        Public Property BEAMLINE As String
+        Public Property DETECTOR_SN As Integer
+        Public Property [DATE] As String
+        Public Property TIME As Double
+        Public Property DISTANCE As Double
+        Public Property TWOTHETA As Double
+        Public Property PHI As Double
+        Public Property OSC_START As Double
+        Public Property OSC_RANGE As Double
+        Public Property WAVELENGTH As Double
+        Public Property BEAM_CENTER_X As Double
+        Public Property BEAM_CENTER_Y As Double
+        Public Property DENZO_X_BEAM As Double
+        Public Property DENZO_Y_BEAM As Double
 
         Sub New()
         End Sub
 
-        Public Shared Function Load(metadata As Dictionary(Of String, String)) As Header
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
 
+        Public Shared Function Load(metadata As Dictionary(Of String, String)) As Header
+            Dim header As New Header
+
+            Static writers As Dictionary(Of String, PropertyInfo) = GetType(Header) _
+                .GetProperties(PublicProperty) _
+                .Where(Function(pi) pi.CanWrite AndAlso pi.GetIndexParameters.IsNullOrEmpty) _
+                .ToDictionary(Function(pi)
+                                  Return pi.Name
+                              End Function)
+
+            For Each tuple As KeyValuePair(Of String, String) In metadata
+                If writers.ContainsKey(tuple.Key) Then
+                    writers(tuple.Key).SetValue(header, value:=any.CTypeDynamic(
+                            expression:=tuple.Value,
+                            target:=writers(tuple.Key).PropertyType
+                        )
+                    )
+                End If
+            Next
+
+            Return header
         End Function
     End Class
 End Namespace
--- src/SMVFile/FileFormat/Reader.vb
+++ src/SMVFile/FileFormat/Reader.vb
@@ -6,15 +6,16 @@ Imports Microsoft.VisualBasic.Text.Parser
 
 Namespace FileFormat
 
-    Public Class Reader
+    Public Class Reader : Implements IDisposable
 
         ReadOnly buf As BinaryDataReader
-        ReadOnly header_size As Integer
 
+        Dim header_size As Integer
         Dim header As Header
+        Private disposedValue As Boolean
 
         Sub New(file As Stream)
-            buf = New BinaryDataReader(file)
+            buf = New BinaryDataReader(file, Encodings.ASCII)
         End Sub
 
         Public Function ReadHeader() As Header
@@ -24,6 +25,11 @@ Namespace FileFormat
             End If
 
             Return header
+        End Function
+
+        Public Function ReadData()
+            Call buf.Seek(header_size, SeekOrigin.Begin)
+
         End Function
 
         Private Sub loadHeaderInternal()
@@ -39,19 +45,48 @@ Namespace FileFormat
                     str += c
                 Else
                     If str = "{" OrElse str = "}" Then
-                        str *= 0
+                        ' do nothing
                     Else
                         tuple = str.ToString.GetTagValue("=", trim:=True)
-                        metadata.Add(tuple.Name, tuple.Value)
+                        metadata.Add(tuple.Name, tuple.Value.TrimEnd(";"c, " "c, ASCII.TAB))
                     End If
+
+                    str *= 0
                 End If
 
-                If buf.Position = header_size AndAlso c = "}"c Then
+                If c = "}"c Then
                     Exit Do
                 End If
             Loop
 
             header = Header.Load(metadata)
+            header_size = header.header_bytes
+        End Sub
+
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: 释放托管状态(托管对象)
+                    Call buf.Dispose()
+                End If
+
+                ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                ' TODO: 将大型字段设置为 null
+                disposedValue = True
+            End If
+        End Sub
+
+        ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+        ' Protected Overrides Sub Finalize()
+        '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        '     Dispose(disposing:=False)
+        '     MyBase.Finalize()
+        ' End Sub
+
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+            Dispose(disposing:=True)
+            GC.SuppressFinalize(Me)
         End Sub
     End Class
 End Namespace
